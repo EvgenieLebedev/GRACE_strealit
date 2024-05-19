0441024d0eff4afb97b2129d933f93d95c764e81
EvgenieLebedev <92586907+EvgenieLebedev@users.noreply.github.com>
2024-05-19 20:41:39 +0300

    7
    7

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ def extract_dates_from_filenames(directory):
     for filename in filenames:
         parts = filename.split("_")
         if len(parts) == 4 and parts[0] == "GRACE":
-            date_string = "_".join(parts[1:]).split(".")[0]  # Убираем расширение файла
+            date_string = "_".join(parts[1:]).split(".")[0]  
             dates.append(date_string)
+    dates.sort()  
     return dates
 
 images_dir = "GRACE"
@@ -18,14 +19,12 @@ dates = extract_dates_from_filenames(images_dir)
 st.title("Визуализация данных GRACE")
 st.write("Этот проект выполнен Лебедевым Е.Д. в рамках магистерской ВКР по исследованию поля силы тяжести")
 
-# Создаем выпадающий список для выбора даты
+
 selected_date = st.selectbox('Выберите дату', dates)
 
-# Формируем имя файла выбранного изображения
 selected_image = f"GRACE_{selected_date}.png"
 image_path = os.path.join(images_dir, selected_image)
 
-# Отображаем изображение
 if os.path.exists(image_path):
     st.image(image_path, caption=f"Данные GRACE за {selected_date}", use_column_width=True)
 else:
